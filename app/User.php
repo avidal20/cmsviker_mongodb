8b2d57e8430ec6e6b570069fb78dfc809bbde9d1
Andres Felipe Vidal <soldider1621@gmail.com>
2017-06-23 21:02:41 -0500

    Inicio de sesion con usuario

--- app/User.php
+++ app/User.php
@@ -23,7 +23,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','username',
     ];
 
     /**
--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -15,5 +15,11 @@ return [
 
     'failed'   => 'Estas credenciales no coinciden con nuestros registros.',
     'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
+    'fielset_title_login' => 'Acceder a Yubarta',
+    'field_login_user' => 'Usuario',
+    'field_login_password' => 'Contrase&ntilde;a',
+    'btn_login_start' => 'Iniciar',
+    'remember_me' => 'Recu&eacute;rdame',
+    'remember_password' => 'Olvidaste tu contrase&ntilde;a?',
 
 ];
